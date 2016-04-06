d3a2a9730c937e30783bef89db25d0c85c73e6f5
Stephan Jaschke <stephan@stj-online.de>
2016-04-06 07:59:33 +0200

    Updated package with correct path; Change configuration to rebuild with OpenSSL, no debugging

--- laz_synapse.pas
+++ laz_synapse.pas
@@ -2,23 +2,23 @@
   This source is only used to compile and install the package.
  }
 
-unit laz_synapse; 
+unit laz_synapse;
 
 interface
 
 uses
-    asn1util, blcksock, clamsend, dnssend, ftpsend, ftptsend, httpsend, 
+  asn1util, blcksock, clamsend, dnssend, ftpsend, ftptsend, httpsend, 
   imapsend, ldapsend, mimeinln, mimemess, mimepart, nntpsend, pingsend, 
-  pop3send, slogsend, smtpsend, snmpsend, sntpsend, synachar, synacode, 
-  synacrypt, synadbg, synafpc, synaicnv, synaip, synamisc, synaser, synautil, 
-  synsock, tlntsend, LazarusPackageIntf;
+  pop3send, slogsend, smtpsend, snmpsend, sntpsend, ssl_openssl, 
+  ssl_openssl_lib, synachar, synacode, synacrypt, synadbg, synafpc, synaicnv, 
+  synaip, synamisc, synaser, synautil, synsock, tlntsend, LazarusPackageIntf;
 
 implementation
 
-procedure Register; 
+procedure Register;
 begin
-end; 
+end;
 
 initialization
-  RegisterPackage('laz_synapse', @Register); 
+  RegisterPackage('laz_synapse', @Register);
 end.
